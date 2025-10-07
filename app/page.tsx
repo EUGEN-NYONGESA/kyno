9001fc4ce25932f63a8ad6f60917b0c2b37aa189
eugenyongesa@gmail.com <eugenyongesa@gmail.com>
2025-10-07 23:33:06 +0300

    Implement Home page, Clerk Integration, Shadcn, Supabase integration, Clerk Billing, Companion form

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,50 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import Cta from "@/components/CTA";
+import { recentSessions } from "@/constants";
 
 function page() {
   return (
-    <div>
-      <h1>LMS App</h1>
-      <Button className='text-2xl underline'>Get Started</Button>
-    </div>
-  )
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="The Good Shepherd"
+          topic="The 'I Am' Statements of Jesus"
+          subject="C.R.E"
+          duration={30}
+          color="#22C55E"
+        />
+        <CompanionCard
+          id="124"
+          name="The Algorithmic Mind"
+          topic="Demystifying AI & Machine Learning"
+          subject="IT & Computer Science"
+          duration={30}
+          color="#D1D5DB"
+        />
+        <CompanionCard
+          id="125"
+          name="The Wired Brain"
+          topic="Neuroscience of Learning & Well-being"
+          subject="Biology & Physics"
+          duration={30}
+          color="#3B82F6"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionList
+          title="Recently Completed Sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <Cta />
+      </section>
+    </main>
+  );
 }
 
-export default page
+export default page;
--- app/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function page() {
-  return (
-    <div>Sign In</div>
-  )
-}
-
-export default page
